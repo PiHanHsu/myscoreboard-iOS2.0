6a32999bae2a6b03e75d7e9eadcba9c2a41c8079
PiHan Hsu <pihanhsu@gmail.com>
2016-05-15 23:12:19 +0800

    Initial Commit

---
+++ myscoreboard2.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  myscoreboard2.0
+//
+//  Created by PiHan Hsu on 5/15/16.
+//  Copyright © 2016 PiHan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
