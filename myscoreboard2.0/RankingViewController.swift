5dca3b87015fa3cb3771c2c91b2774418fca2c34
PiHan Hsu <pihanhsu@gmail.com>
2016-05-30 23:37:32 +0800

    clean rankingCollection code, set all layout in code

--- myscoreboard2.0/RankingViewController.swift
+++ myscoreboard2.0/RankingViewController.swift
@@ -10,64 +10,83 @@ import UIKit
 
 class RankingViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    let TRANSFORM_CELL_VALUE = CGAffineTransformMakeScale(0.8, 0.8)
+    let TRANSFORM_CELL_VALUE = CGAffineTransformMakeScale(0.9, 0.9)
     let ANIMATION_SPEED = 0.2
     var isfirstTimeTransform = true
-    var widthSize:CGFloat = 0.0
+    var widthSize:CGFloat?
+    var heightSize:CGFloat?
+    var spacing:CGFloat?
     
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //        HttpManager.sharedInstance
-        //            .request(
-        //                HttpMethod.HttpMethodGet,
-        //                apiFunc: APiFunction.GetRanking,
-        //                param: ["auth_token": CurrentUser.sharedInstance.authToken!],
-        //                success: { (code, data ) in
-        //                    print("success")
-        //                    print(data)
-        //                    //self.success(code, data: data)
-        //                }, failure: { (code, data) in
-        //                    //self.failure(code!, data: data!)
-        //                }, complete: nil)
-        // Do any additional setup after loading the view.
+//                HttpManager.sharedInstance
+//                    .request(
+//                        HttpMethod.HttpMethodGet,
+//                        apiFunc: APiFunction.GetRanking,
+//                        param: ["auth_token": CurrentUser.sharedInstance.authToken!],
+//                        success: { (code, data ) in
+//                            print("success")
+//                            print(data)
+//                            //self.success(code, data: data)
+//                        }, failure: { (code, data) in
+//                            //self.failure(code!, data: data!)
+//                        }, complete: nil)
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBOutlet weak var collectionView: UICollectionView!
     
-    
-    //計算每個 Cell 的大小，大小會決定一排會呈現幾個 Cell
     func collectionView(collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        widthSize = self.collectionView.frame.size.width - 150
-        let heightSize = self.collectionView.frame.size.height - 200
-        //print("cellWidth: \(widthSize)")
+        widthSize = self.collectionView.frame.size.width * 0.7
+        heightSize = self.collectionView.frame.size.height * 0.75
         
-        return CGSize.init(width: widthSize, height: heightSize)
-        //return CGSizeMake(300, 300)
+        return CGSize.init(width: widthSize!, height: heightSize!)
     }
     
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAtIndex section: Int) -> UIEdgeInsets{
+        let inset = (self.collectionView.frame.size.width - widthSize!) / 2 - 8
+        
+        return UIEdgeInsets(top: 0, left: inset, bottom: 0, right: inset)
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                               minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat{
+        switch self.view.frame.size.width {
+        case 320:
+            spacing = 2
+        case 375:
+            spacing = 10
+        default:
+            spacing = 15
+        }
+        
+        return spacing!
+    }
     
     // MARK: CollectionView Data Source
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //return Teams.sharedInstance.teams.count + 1
-        return 15
+        return Teams.sharedInstance.teams.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("rankingCollectionViewCell", forIndexPath: indexPath) as! RankingCollectionViewCell
-       
+        
         if (indexPath.row == 0 && isfirstTimeTransform) { // make a bool and set YES initially, this check will prevent fist load transform
             isfirstTimeTransform = false
         }else{
-            print("before: \(cell.frame.size.width)")
+            //print("before: \(cell.frame.size.width)")
             cell.transform = TRANSFORM_CELL_VALUE
-            print("after: \(cell.frame.size.width)")
+            //print("after: \(cell.frame.size.width)")
             // the new cell will always be transform and without animation
         }
         return cell
@@ -75,10 +94,7 @@ class RankingViewController: UIViewController, UICollectionViewDataSource, UICol
     
     func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         
-        let cellWidth: Float = Float(widthSize)
-        print("scrollView cellWidth: \(cellWidth)")
-        let pageWidth = Float(cellWidth + 10)
-        print("pageWidth: \(pageWidth)")
+        let pageWidth = Float(widthSize! + spacing!)
         let currentOffset = Float(scrollView.contentOffset.x)
         let targetOffset = Float(targetContentOffset.memory.x)
         var newTargetOffset = Float(0)
@@ -91,7 +107,6 @@ class RankingViewController: UIViewController, UICollectionViewDataSource, UICol
             newTargetOffset = floorf(currentOffset / pageWidth) * pageWidth
         }
         
-        
         if newTargetOffset < 0{
             newTargetOffset = 0
         }else if newTargetOffset > scrollViewWidth{
@@ -100,23 +115,19 @@ class RankingViewController: UIViewController, UICollectionViewDataSource, UICol
         
         print("newTargetOffset:\(newTargetOffset)")
         targetContentOffset.memory = CGPointMake( CGFloat(currentOffset) , 0)
-        //print("targetContentOffset:\(targetContentOffset.memory.x)")
         scrollView.setContentOffset(CGPointMake(CGFloat(newTargetOffset), 0), animated: true)
         
         let index = Int(newTargetOffset / pageWidth)
-        //print("index: \(index)")
         if index == 0 {
             let cell = self.collectionView.cellForItemAtIndexPath(NSIndexPath(forItem: index, inSection: 0)) as! RankingCollectionViewCell
             
             UIView.animateWithDuration(ANIMATION_SPEED) {
-                print("index0 normal")
                 cell.transform = CGAffineTransformIdentity
             }
             
             if let cell = self.collectionView.cellForItemAtIndexPath(NSIndexPath(forItem: index + 1, inSection: 0)) {
                 
                 UIView.animateWithDuration(ANIMATION_SPEED) {
-                    print("right")
                     cell.transform = self.TRANSFORM_CELL_VALUE
                 }
             }
@@ -134,7 +145,6 @@ class RankingViewController: UIViewController, UICollectionViewDataSource, UICol
             if let cell = self.collectionView.cellForItemAtIndexPath(NSIndexPath(forItem: index - 1, inSection: 0)) {
                 
                 UIView.animateWithDuration(ANIMATION_SPEED) {
-                    print("left")
                     cell.transform = self.TRANSFORM_CELL_VALUE
                 }
             }
@@ -143,7 +153,6 @@ class RankingViewController: UIViewController, UICollectionViewDataSource, UICol
             if let cell = self.collectionView.cellForItemAtIndexPath(NSIndexPath(forItem: index + 1, inSection: 0)) {
                 
                 UIView.animateWithDuration(ANIMATION_SPEED) {
-                    print("right")
                     cell.transform = self.TRANSFORM_CELL_VALUE
                 }
             }
