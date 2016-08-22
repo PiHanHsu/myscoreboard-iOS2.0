f57305a0a9e21652f33bf68e411e5ab13ceb5d87
PiHan Hsu <pihanhsu@gmail.com>
2016-08-22 20:36:06 +0800

    Add refresh control on Today's ranking Add single game cell on Today's ranking page

--- myscoreboard2.0/TodayRankTableViewController.swift
+++ myscoreboard2.0/TodayRankTableViewController.swift
@@ -19,27 +19,17 @@ class TodayRankTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.registerNib(UINib(nibName: gameTableViewCell, bundle: nil), forCellReuseIdentifier: gameTableViewCell)
+        self.tableView.registerNib(UINib(nibName: "SingleGameTableViewCell", bundle: nil), forCellReuseIdentifier: "SingleGameTableViewCell")
         
-        HttpManager.sharedInstance
-            .request(HttpMethod.HttpMethodPost,
-                     apiFunc: APiFunction.GetTodayGames,
-                     param: ["auth_token" : CurrentUser.sharedInstance.authToken!,
-                        "team_id": team.teamId!],
-                     success: { (code , data ) in
-                       //print(data)
-                       self.gameData = data["result"]["today_games"]
-                       print("count: \(self.gameData.count)")
-                       print(self.gameData)
-                       self.tableView.reloadData()
-                },
-                     failure: { (code , data) in
-                        print("failed with \(code), \(data)")
-                },
-                     complete: nil)
-
+        refreshControl = UIRefreshControl()
+        //refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl!.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl!)
         
+        loadingData()
+    
     }
-
+    
     override func viewDidAppear(animated: Bool) {
         UXTestingManager.sharedInstance().heatMapStartWithViewName("Today's Rank Page")
     }
@@ -47,32 +37,34 @@ class TodayRankTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return gameData.count
     }
-
+    
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 60
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(gameTableViewCell, forIndexPath: indexPath) as! GameTableViewCell
+        
         
         let oneGameData = gameData[indexPath.row]["game"]
-       
+        
         if oneGameData.count == 2 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("SingleGameTableViewCell", forIndexPath: indexPath) as! SingleGameTableViewCell
+            
             cell.team1Player1NameLabel.text = oneGameData[0]["username"].stringValue
-            cell.team1Player2NameLabel.text = ""
+            
             cell.team2Player1NameLabel.text = oneGameData[1]["username"].stringValue
-            cell.team2Player2NameLabel.text = ""
+            
             if oneGameData[0]["score"].intValue > oneGameData[1]["score"].intValue {
                 cell.team1ScoreLabel.textColor = UIColor.redColor()
             }else {
@@ -80,7 +72,10 @@ class TodayRankTableViewController: UITableViewController {
             }
             cell.team1ScoreLabel.text = oneGameData[0]["score"].stringValue
             cell.team2ScoreLabel.text = oneGameData[1]["score"].stringValue
+            
+            return cell
         }else {
+            let cell = tableView.dequeueReusableCellWithIdentifier(gameTableViewCell, forIndexPath: indexPath) as! GameTableViewCell
             cell.team1Player1NameLabel.text = oneGameData[0]["username"].stringValue
             cell.team1Player2NameLabel.text = oneGameData[1]["username"].stringValue
             cell.team2Player1NameLabel.text = oneGameData[2]["username"].stringValue
@@ -92,55 +87,83 @@ class TodayRankTableViewController: UITableViewController {
             }
             cell.team1ScoreLabel.text = oneGameData[0]["score"].stringValue
             cell.team2ScoreLabel.text = oneGameData[2]["score"].stringValue
+            
+            return cell
+            
         }
         
-        return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func refresh(sender:AnyObject) {
+        loadingData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func loadingData() {
+        
+        HttpManager.sharedInstance
+            .request(HttpMethod.HttpMethodPost,
+                     apiFunc: APiFunction.GetTodayGames,
+                     param: ["auth_token" : CurrentUser.sharedInstance.authToken!,
+                        "team_id": team.teamId!],
+                     success: { (code , data ) in
+                        //print(data)
+                        self.gameData = data["result"]["today_games"]
+                        //print("count: \(self.gameData.count)")
+                        //print(self.gameData)
+                        self.refreshControl!.endRefreshing()
+                        self.tableView.reloadData()
+                },
+                     failure: { (code , data) in
+                        self.refreshControl!.endRefreshing()
+                        print("failed with \(code), \(data)")
+                },
+                     complete: nil)
     }
-    */
-
+    
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
